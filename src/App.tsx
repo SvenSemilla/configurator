31310a129e9a569f25cd45f2c72e775def46e920
SvenSemilla <katapulsky@yahoo.de>
2026-01-15 20:32:03 +0100

    Standalone Konfigurator Version 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,27 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { CartProvider } from "@/context/CartContext";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import Index from "./pages/Index";
-import Katalog from "./pages/Katalog";
-import ProductDetail from "./pages/ProductDetail";
-import Stoffe from "./pages/Stoffe";
-import Anfrage from "./pages/Anfrage";
-import Warenkorb from "./pages/Warenkorb";
 import Konfigurator from "./pages/Konfigurator";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <CartProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
           <div className="flex flex-col min-h-screen">
-            <Header />
             <main className="flex-1">
               <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/katalog" element={<Katalog />} />
-                <Route path="/produkt/:slug" element={<ProductDetail />} />
-                <Route path="/stoffe" element={<Stoffe />} />
-                <Route path="/anfrage" element={<Anfrage />} />
-                <Route path="/warenkorb" element={<Warenkorb />} />
-                <Route path="/konfigurator" element={<Konfigurator />} />
-                <Route path="*" element={<NotFound />} />
+                {/* Der Konfigurator ist jetzt die einzige und primäre Seite */}
+                <Route path="/" element={<Konfigurator />} />
+                {/* Fallback: Alles andere führt auch zum Konfigurator */}
+                <Route path="*" element={<Konfigurator />} />
               </Routes>
             </main>
-            <Footer />
           </div>
         </BrowserRouter>
-      </CartProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
